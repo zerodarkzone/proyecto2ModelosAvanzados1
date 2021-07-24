b363c856c4652627853456f0b88708e9de74b280
Juan Barreto <jdbarretop@unal.edu.co>
2021-07-24 10:35:07 -0500

    first api test

--- app/main.py
+++ app/main.py
@@ -3,6 +3,19 @@ from flask import Flask
   
 app = Flask(__name__)
   
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home_view():
-        return "<h1>Hello world</h1>"
+    return "<h1>Hello world</h1>"
+		
+@app.route("/predict", methods=['GET'])
+def predict():
+	try:
+		year = int(request.args.get('year'))
+		mileage = int(request.args.get('mileage'))
+		state = request.args.get('state')
+		make = request.args.get('make')
+		model = request.args.get('model')
+		return {"cost": 25000}
+	except Exception as e:
+		return {"error": "Invalid parameters"}
+		
